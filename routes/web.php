a899efab0aa3999c1c95a109ad6b8861eadb4fee
shofa yasmin fauziah <yasminshofa.sy@gmail.com>
2024-02-29 23:46:10 +0700

    js2

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,11 @@ use App\Http\Controllers\BabyKidController;
 use App\Http\Controllers\BeautyHealthController;
 use App\Http\Controllers\FoodBaverageController;
 use App\Http\Controllers\HomeCareController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\SalesPageController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,9 +18,51 @@ Route::get('/homePage', function () {
     return ('Welcome To Point of Sales Application');
 });
 
+//Display porduct list(route prefix)
 Route::prefix('category')->group(function () {
     Route::get('/food-baverage', [FoodBaverageController::class, 'foodBaverage']);
     Route::get('/beauty-health', [BeautyHealthController::class, 'beautyHealth']);
     Route::get('/home-care', [HomeCareController::class, 'homeCare']);
     Route::get('/baby-kid', [BabyKidController::class, 'babyKid']);
 });
+
+//Displaying food-baverage using View
+Route::get('/foodBaverage2', function () {
+    return view('blog.foodBaverage');
+});
+
+//Displaying beauty-health using View
+Route::get('/beautyHealth2', function () {
+    return view('blog.beautyHealth');
+});
+
+//Displaying home-care using View
+Route::get('/homeCare2', function () {
+    return view('blog.homeCare');
+});
+
+//Displaying baby-kid using View
+Route::get('/babyKid2', function () {
+    return view('blog.babyKid');
+});
+
+
+//Display user profiles(route parameter)
+Route::get('/user/{id}/name/{name}', function ($id, $name){
+    return 'User ke-'. $id." Nama : ".$name;
+});
+//Using controller
+Route::get('/user-profile/{id}/{name}', [PageController::class,'userProfile']);
+
+//Displaying a View from a Controller
+Route::get('/user-profile2', [PageController::class,'userProfile2']);
+
+//Sales Page using view 
+Route::get('/sales-page', function () {
+    return view('blog.salesPage');
+});
+
+//Sales Page using View from a Controller
+Route::get('/sales-page', [SalesPageController::class,'salesPage']);
+
+
