d9966b42dd75dcd037f98cb9be446ffff614efc7
shofa yasmin fauziah <yasminshofa.sy@gmail.com>
2024-02-29 23:45:24 +0700

    js2

---
+++ app/Http/Controllers/SalesPageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SalesPageController extends Controller
+{
+    public function salesPage(){
+        return 'Sales Page';
+    }
+
+}
